75ab5bd76691f7a9d1daf2f903d631eca69ae3b4
thang44hdai <thangquang8888@gmail.com>
2023-12-12 12:29:52 +0700

    .

--- app/src/main/java/com/example/note/data/EntertainmentItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.note.data
-
-data class EntertainmentItem(
-    val link: String,
-    val title: String,
-    val image: String,
-)
---
+++ app/src/main/java/com/example/note/presentation/tutorial/TutorialActivity.kt
@@ -0,0 +1,15 @@
+package com.jp.xronos.ui.tutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jp.xronos.R
+import com.jp.xronos.databinding.ActivityTutorialBinding
+
+class TutorialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+
+
+    }
+}
